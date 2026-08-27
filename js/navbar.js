3953e35b293e3f4596a1eed3a0d92ae72c792c5c
imaliarosyidaa <imaliarosyidaa@gmail.com>
2026-08-27 18:41:42 +0700

    perbaiki responsifitas home

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,5 @@
-function appendNavbar(){
-    const nav = `
+function appendNavbar() {
+  const nav = `
       <div class="navbar-container">
         <button id="menuBtn" class="openSidebarBtn hamburger-btn">
           <svg
@@ -37,14 +37,16 @@ function appendNavbar(){
           <a id="openCartBtn" class="col"
             ><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-shopping-cart-icon lucide-shopping-cart"><circle cx="8" cy="21" r="1"/><circle cx="19" cy="21" r="1"/><path d="M2.05 2.05h2l2.66 12.42a2 2 0 0 0 2 1.58h9.78a2 2 0 0 0 1.95-1.57l1.65-7.43H5.12"/></svg></a>
                   <div style="display: flex; gap: 12px;">
-        <a href="login.html" class="button-link-outline-navbar">Masuk</a>
+        <div><a href="login.html" class="button-link-outline-navbar">Masuk</a></div>
+        <div>
                   <a href="register.html" class="button-link-navbar">Daftar</a>
+                  </div>
       </div>
         </div>
       </div>
     `
-    const target = document.querySelector('.custom-navbar');
-    if (target) target.innerHTML = nav;
+  const target = document.querySelector('.custom-navbar');
+  if (target) target.innerHTML = nav;
 }
 
 appendNavbar()
